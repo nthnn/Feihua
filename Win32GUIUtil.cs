3bbcc29ec8e9b39d7d075d7af65492443302daf5
Nathanne Isip <nathanneisip@gmail.com>
2024-07-15 23:46:04 +0800

    Win32 GUI native invocation interface.

---
+++ Win32GUIUtil.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Copyright 2024 Nathanne Isip
+ * 
+ * Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”),
+ * to deal in the Software without restriction,
+ * including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions
+ * of the Software.
+ */
+
+using System.Runtime.InteropServices;
+
+namespace Feihua
+{
+    public class Win32GUIUtil
+    {
+        public enum Bool
+        {
+            False = 0,
+            True
+        };
+
+        public const Int32 ULW_COLORKEY = 0x00000001;
+        public const Int32 ULW_ALPHA = 0x00000002;
+        public const Int32 ULW_OPAQUE = 0x00000004;
+
+        public const byte AC_SRC_OVER = 0x00;
+        public const byte AC_SRC_ALPHA = 0x01;
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int EM_SETCUEBANNER = 0x1501;
+        public const int HT_CAPTION = 0x2;
+
+        public const int AW_HOR_POSITIVE = 0X1;
+        public const int AW_HOR_NEGATIVE = 0X2;
+        public const int AW_VER_POSITIVE = 0X4;
+        public const int AW_VER_NEGATIVE = 0X8;
+        public const int AW_CENTER = 0X10;
+        public const int AW_HIDE = 0X10000;
+        public const int AW_ACTIVATE = 0X20000;
+        public const int AW_SLIDE = 0X40000;
+        public const int AW_BLEND = 0X80000;
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct BLENDFUNCTION
+        {
+            public byte BlendOp;
+            public byte BlendFlags;
+            public byte SourceConstantAlpha;
+            public byte AlphaFormat;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Point
+        {
+            public Int32 x;
+            public Int32 y;
+
+            public Point(Int32 x, Int32 y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Size
+        {
+            public Int32 cx;
+            public Int32 cy;
+
+            public Size(Int32 cx, Int32 cy)
+            {
+                this.cx = cx;
+                this.cy = cy;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern Int32 SendMessage(IntPtr hWnd, int msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int AnimateWindow(IntPtr hwand, int dwTime, int dwFlags);
+    }
+}
